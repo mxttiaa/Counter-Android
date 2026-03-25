512cc9968b33f9f275b01a1044da30540495529d
Mattia <mattia1luigi0107@gmail.com>
2026-03-25 18:45:47 +0100

    tema e cambio sfondo ogni 10

--- app/src/main/java/com/mxttiaa/mcounter/MainActivity.java
+++ app/src/main/java/com/mxttiaa/mcounter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mxttiaa.mcounter;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
     int value = 0;
     int gap = 1;
+
+    int[] lightbackg = {
+            R.color.honeydew,
+            R.color.lightStrawberry,
+            R.color.lightSteelBlue,
+            R.color.lightSand,
+            R.color.lightLavender,
+            R.color.lightMint
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,19 @@ public class MainActivity extends AppCompatActivity {
             if(value + gap <= 99999){
                 value += gap;
                 textValue.setText(String.valueOf(value)); //si può fare anche value + ""
+
+                View mainLayout = findViewById(R.id.main); //Colleghiamo il layout principale per potergli cambiare colore
+
+                if (value > 0 && value % 10 == 0) {
+                    java.util.Random random = new java.util.Random(); //oggetto random
+
+                    int idxCoolor = random.nextInt(lightbackg.length); //numero casuale tra 0 e colori totali
+
+                    int myColor = lightbackg[idxCoolor]; //colore estratto
+
+                    // Nota: in Android moderno serve "ContextCompat" per tradurre il colore in un formato leggibile dallo schermo
+                    mainLayout.setBackgroundColor(androidx.core.content.ContextCompat.getColor(MainActivity.this, myColor)); //applicazione del colore sullo sfondo
+                }
             }
             else{
                 Toast.makeText(MainActivity.this, "Hai raggiunto il massimo.", Toast.LENGTH_SHORT).show();
