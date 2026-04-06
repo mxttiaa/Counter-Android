025fdf7edf4592019ab465d57b42549de7a22f1f
Mattia <mattia1luigi0107@gmail.com>
2026-04-06 18:25:31 +0200

    Toolbar creata

--- app/src/main/java/com/mxttiaa/mcounter/SettingsActivity.java
+++ app/src/main/java/com/mxttiaa/mcounter/SettingsActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +21,19 @@ public class SettingsActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbarSettings);
+        setSupportActionBar(toolbar);
+
+        // Questo abilita la freccetta in alto a sinistra
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Questo dice cosa fare quando la freccia viene cliccata
+        toolbar.setNavigationOnClickListener(v -> {
+            finish(); // <--- LA PAROLA MAGICA: chiude l'Activity e torna "sotto"
         });
     }
 }
